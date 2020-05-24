d344a2fc4817381d3faaa46281560f103ad93d10
jaravad <jaravad@uninorte.edu.co>
2020-05-23 21:09:06 -0500

    villains details

--- src/app/components/hero-details/hero-details.component.ts
+++ src/app/components/hero-details/hero-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService, Hero } from '../../service/hero.service';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-details',
@@ -13,13 +14,17 @@ export class HeroDetailsComponent implements OnInit {
 
   constructor(
     private _activeRoute: ActivatedRoute,
-    private _heroService: HeroService
+    private _heroService: HeroService,
+    private _location: Location
   ) {
     this._activeRoute.params.subscribe((params) => {
       //console.log(params['id']);
-      this.hero = this._heroService.getHero(params['id']);
-      console.log(this.hero);
+      this.hero = this._heroService.getCharacter(params['id']);
     });
+  }
+
+  back() {
+    this._location.back();
   }
   ngOnInit(): void {}
 }
